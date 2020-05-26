4497b5df6e159d7d37dbc7f6df5c891a410a03bb
stephaneworkspace <s.bressani@bluewin.ch>
2020-05-26 19:54:54 +0200

    add logo size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img class="logo" alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -16,3 +16,10 @@ export default {
   }
 };
 </script>
+
+<style>
+.logo {
+  width: 600px;
+  height: 280px;
+}
+</style>
